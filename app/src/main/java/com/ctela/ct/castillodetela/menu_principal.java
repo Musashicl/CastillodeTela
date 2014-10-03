294ef00bd0548674535913899a300d2856368fc8
PainkilleRcl <ssalinasro@gmail.com>
2014-10-03 00:33:38 -0300

    version 0.1
    algunos errores corregidos y implementacion de scroll para los cuentos,
    se estan creando dinamicos.
    PENDIENTE:
    - manejo de cuentos como modulos
    - sistema de descarga
    - agregar cuentos.
    ..etc

--- app/src/main/java/com/ctela/ct/castillodetela/menu_principal.java
+++ app/src/main/java/com/ctela/ct/castillodetela/menu_principal.java
@@ -1,22 +1,49 @@
 package com.ctela.ct.castillodetela;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ctela.ct.castillodetela.view.VerticalPager;
+
 
 /**
  * Created by benja on 24-09-2014.
  */
-public class menu_principal extends ActionBarActivity {
+public class menu_principal extends Activity {
 
+    VerticalPager pagerLibros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_menu);
+        pagerLibros = (VerticalPager) findViewById(R.id.pgLibros);
+
+        for (int i = 0; i < 20; i++) {
+            setViewPagerData("Subhanallah " + i);
+        }
     }
 
+    private void setViewPagerData(final String str) {
+        Log.e("Set Filpper Called", str + "");
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.flipper_item_view, null);
+        TextView textView1 = (TextView) view.findViewById(R.id.textView1);
+        TextView textView2 = (TextView) view.findViewById(R.id.textView2);
+        textView1.setText("" + str);
+        textView2.setText("" + str);
+        Button button = (Button) view.findViewById(R.id.button1);
+        pagerLibros.addView(view);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,6 +51,16 @@ public class menu_principal extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.splash_screen, menu);
         return true;
     }
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/ctela/ct/castillodetela/new_splash.java
+++ app/src/main/java/com/ctela/ct/castillodetela/new_splash.java
@@ -5,25 +5,35 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+
+
 
 /**
  * Created by benja on 26-09-2014.
  */
 public class new_splash extends Activity {
 
-    ImageView ivNube1,ivNube2,ivNube3;
+    ImageView ivNube1,ivNube2,ivNube3,btInicio;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_newsplash);
 
         ivNube1 = (ImageView) findViewById(R.id.ivNube1);
         ivNube2 = (ImageView) findViewById(R.id.ivNube2);
-        //ivNube3 = (ImageView) findViewById(R.id.ivNube3);
+        ivNube3 = (ImageView) findViewById(R.id.ivNube3);
+        btInicio = (ImageView) findViewById(R.id.btInicio);
 
         //rotate nube1
         ObjectAnimator rotateNube1 = ObjectAnimator.ofFloat(ivNube1, View.ROTATION, 3);
@@ -36,11 +46,15 @@ public class new_splash extends Activity {
         rotateNube2.setDuration(1000);
         rotateNube2.setRepeatMode(ValueAnimator.REVERSE);
         //rotate nube 3
-       /* ObjectAnimator rotateNube3 = ObjectAnimator.ofFloat(ivNube3, View.ROTATION, 3);
+        ObjectAnimator rotateNube3 = ObjectAnimator.ofFloat(ivNube3, View.ROTATION, 3);
         rotateNube3.setRepeatCount(ValueAnimator.INFINITE);
         rotateNube3.setDuration(1000);
         rotateNube3.setRepeatMode(ValueAnimator.REVERSE);
-*/
+
+
+
+
+
        // Translate nube 1
         ObjectAnimator translateNube1 = ObjectAnimator.ofFloat(ivNube1, View.TRANSLATION_X, 800);
         translateNube1.setRepeatCount(ValueAnimator.INFINITE);
@@ -54,12 +68,15 @@ public class new_splash extends Activity {
         translateNube3.setRepeatCount(ValueAnimator.INFINITE);
         translateNube3.setDuration(15000);
 
+        ObjectAnimator rotateInicio = ObjectAnimator.ofFloat(btInicio, View.ROTATION, 4);
+        rotateInicio.setRepeatCount(ValueAnimator.INFINITE);
+        rotateInicio.setDuration(1500).setRepeatMode(ValueAnimator.REVERSE);
 
 
 
 
         AnimatorSet setAnimation = new AnimatorSet();
-       setAnimation.play(rotateNube1).with(rotateNube2).with(translateNube1).with(translateNube2);
+        setAnimation.play(rotateNube1).with(rotateNube2).with(translateNube1).with(translateNube2).with(rotateNube3).with(translateNube3).with(rotateInicio);
         setAnimation.start();
 
 
@@ -72,6 +89,8 @@ public class new_splash extends Activity {
 
         startActivity(Inicio);
     }
+
+
 
 
 }
